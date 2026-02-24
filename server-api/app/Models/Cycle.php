7e767fd705031971ef1ccac9c62b8a4b200ebe72
Chloe Dwyer <N00220849@iadt.ie>
2026-02-24 20:17:45 +0000

    Updating models to work for testing

--- server-api/app/Models/Cycle.php
+++ server-api/app/Models/Cycle.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Cycle extends Model
 {
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'start_date',
+        'end_date',
+        'cycle_length',
+    ];
+    
     //Relationships
     public function user()
     {
--- server-api/app/Models/Period.php
+++ server-api/app/Models/Period.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Period extends Model
 {
+    use HasFactory;
+
+    protected $fillable = [
+        'cycle_id',
+        'start_date',
+        'end_date',
+        'flow_level',
+        'has_clots',
+    ];
+    
     //Relationships
     public function cycle()
     {
