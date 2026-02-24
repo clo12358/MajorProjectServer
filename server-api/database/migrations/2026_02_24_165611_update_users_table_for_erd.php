3a662f1fe32200f13d629cb4e84a4d0173958e16
Chloe Dwyer <N00220849@iadt.ie>
2026-02-24 17:02:44 +0000

    Updating user table

---
+++ server-api/database/migrations/2026_02_24_165611_update_users_table_for_erd.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->date('dob');
+            $table->float('height')->nullable();
+            $table->float('weight')->nullable();
+            $table->string('profile_image')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn([
+                'dob',
+                'height',
+                'weight',
+                'profile_image'
+            ]);
+        });
+    }
+};
